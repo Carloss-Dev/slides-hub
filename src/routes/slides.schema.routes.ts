c2f8801f60cf0aeb7e3e225ea5b7e710d2d73a60
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-03-24 22:52:20 -0300

    feat: projeto base

---
+++ src/routes/slides.schema.routes.ts
@@ -0,0 +1,15 @@
+import { DevWebIntroductionRoom1 } from "@/slides/geracaoTech";
+import type React from "react";
+
+interface ISlideRoutesSchema {
+  path: string;
+  label?: string;
+  element: React.ElementType;
+}
+
+export const slideRoutesSchema: ISlideRoutesSchema[] = [
+  {
+    path: "/geracao-tech/modulo-1/aula-introdutoria",
+    element: DevWebIntroductionRoom1,
+  },
+];
